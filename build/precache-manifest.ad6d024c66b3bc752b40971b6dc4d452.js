8d465dec438b582c15c1df0b1970b500c7d831e2
pathakabh <39545599+pathakabh@users.noreply.github.com>
2021-07-10 17:10:20 +0530

    build

---
+++ build/precache-manifest.ad6d024c66b3bc752b40971b6dc4d452.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c0f50f4f956092ba7f07d15a164b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d59f10024f6eeca557d",
+    "url": "/static/css/3.a4ff2d2e.chunk.css"
+  },
+  {
+    "revision": "b554a32682189036e927",
+    "url": "/static/css/main.6c38d0be.chunk.css"
+  },
+  {
+    "revision": "93ec9801985a662aab78",
+    "url": "/static/js/0.781f577e.chunk.js"
+  },
+  {
+    "revision": "1d59f10024f6eeca557d",
+    "url": "/static/js/3.ebe1194b.chunk.js"
+  },
+  {
+    "revision": "74e67453b0ecce01b9591396c38d1fed",
+    "url": "/static/js/3.ebe1194b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b554a32682189036e927",
+    "url": "/static/js/main.e1e5f1f0.chunk.js"
+  },
+  {
+    "revision": "47af62539cf1557c3daa",
+    "url": "/static/js/runtime-main.89ce2e39.js"
+  }
+]);
